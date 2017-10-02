84d83d2ed0359ae7e24990c2a0d7a5cd54e68776
Tomochika Hara <tomochikahara@gmail.com>
2017-10-03 02:01:59 +0900

    Documentation

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 aliasmethod
 ====
 
-Implementation of [Walker's Alias method](https://en.wikipedia.org/wiki/Alias_method) by Rust
+[![Build Status](https://travis-ci.org/thara/rust_aliasmethod.svg?branch=master)](https://travis-ci.org/thara/rust_aliasmethod)
+[![](http://meritbadge.herokuapp.com/aliasmethod)](https://crates.io/crates/aliasmethod)
+
+Implementation of [Walker's Alias method](https://en.wikipedia.org/wiki/Alias_method) by Rust.
+
+The algorithm is is principally useful when you need to random sampling with replacement by `O(1)`.
+
+## Example
+
+```rust
+use aliasmethod::{new_alias_table, alias_method}
+
+let weights = vec![1.0, 1.0, 8.0];
+match new_alias_table(weights) {
+    Err(e) => {
+        println!(false, "error : {}", e);
+    }
+    Ok(alias_table) => {
+        let n = alias_method().random(&alias_table);
+        assert!(0 <= n && n <= weights.length);
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
-//! Implementation of [Walker's Alias method](https://en.wikipedia.org/wiki/Alias_method)
+// Implementation of [Walker's Alias method](https://en.wikipedia.org/wiki/Alias_method)
+//
+// The Walker's Alias method algorithm is principally useful when you need to
+// random sampling with replacement by `O(1)`.
+//
+// # Examples
+//
+// ```rust
+// use aliasmethod::{new_alias_table, alias_method}
+//
+// let weights = vec![1.0, 1.0, 8.0];
+// match new_alias_table(weights) {
+//     Err(e) => {
+//         println!(false, "error : {}", e);
+//     }
+//     Ok(alias_table) => {
+//         let n = alias_method().random(&alias_table);
+//         assert!(0 <= n && n <= weights.length);
+// }
+// ```
 extern crate rand;
 
 use std::fmt;
@@ -7,7 +26,7 @@ use self::rand::distributions::{IndependentSample, Range};
 
 
 pub struct AliasMethod<RNG: Rng> {
-    rng: RNG
+    rng: RNG,
 }
 
 
@@ -27,20 +46,21 @@ pub struct AliasTable {
 #[derive(Debug)]
 pub enum AliasMethodError {
     ZeroTotalWeights,
-    Internal{ text: String },
+    Internal {
+        text: String,
+    },
 }
 
 impl fmt::Display for AliasMethodError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             AliasMethodError::ZeroTotalWeights => write!(f, "Total of weights is 0."),
-            AliasMethodError::Internal{ref text} => write!(f, "Internal error: {}", text),
+            AliasMethodError::Internal { ref text } => write!(f, "Internal error: {}", text),
         }
     }
 }
 
 impl<RNG: Rng> AliasMethod<RNG> {
-
     /// Creates a new AliasMethod struct.
     pub fn new(rng: RNG) -> Self {
         AliasMethod { rng: rng }
@@ -67,7 +87,7 @@ pub fn new_alias_table(weights: &Vec<f64>) -> Result<AliasTable, AliasMethodErro
 
     let sum = weights.iter().fold(0.0, |acc, x| acc + x);
     if sum == 0.0 {
-        return Err(AliasMethodError::ZeroTotalWeights)
+        return Err(AliasMethodError::ZeroTotalWeights);
     }
 
     let mut prob = weights.iter().map(|w| w * (n as f64) / sum).collect::<Vec<f64>>();
@@ -93,10 +113,10 @@ pub fn new_alias_table(weights: &Vec<f64>) -> Result<AliasTable, AliasMethodErro
         let k = hl[(h - 1) as usize];
 
         if 1.0 < prob[j] {
-            return Err(AliasMethodError::Internal{text: format!("MUST: {} <= 1", prob[j])})
+            return Err(AliasMethodError::Internal { text: format!("MUST: {} <= 1", prob[j]) });
         }
         if prob[k] < 1.0 {
-            return Err(AliasMethodError::Internal{text: format!("MUST: 1 <= {}", prob[k])})
+            return Err(AliasMethodError::Internal { text: format!("MUST: 1 <= {}", prob[k]) });
         }
 
         a[j] = k;
@@ -119,14 +139,11 @@ pub fn new_alias_table(weights: &Vec<f64>) -> Result<AliasTable, AliasMethodErro
 
 #[test]
 fn test_new_alias_table() {
-    let params = [
-        vec![1.0, 1.0],
-        vec![1.0, 1.0, 8.0],
-    ];
+    let params = [vec![1.0, 1.0], vec![1.0, 1.0, 8.0]];
     for sample_weights in params.into_iter() {
         let alias_table = new_alias_table(&sample_weights);
         match alias_table {
-            Ok(AliasTable {prob, ..}) => {
+            Ok(AliasTable { prob, .. }) => {
                 assert_eq!(prob.len(), sample_weights.len());
             }
             Err(e) => {
